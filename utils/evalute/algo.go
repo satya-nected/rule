15a18a390b6551dd527727d47373c1eebcd50daf
satya-nected <satya@nected.ai>
2023-03-17 15:41:15 +0530

    .

--- utils/evalute/algo.go
+++ utils/evalute/algo.go
@@ -1,13 +1,31 @@
 package evalute
 
 func CheckContainsString(a, b string) bool {
-	return true
+	if a != "" && b != "" {
+		aLen, bLen := len(a), len(b)
+		for i := 0; i <= aLen-bLen; i++ {
+			if a[i:i+bLen] == b {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func CheckStartWithString(a, b string) bool {
-	return true
+	if a != "" && b != "" && len(a) >= len(b) {
+		if b == a[0:len(b)] {
+			return true
+		}
+	}
+	return false
 }
 
 func CheckEndWithString(a, b string) bool {
-	return true
+	if a != "" && b != "" && len(a) >= len(b) {
+		if b == a[len(a)-len(b):] {
+			return true
+		}
+	}
+	return false
 }
